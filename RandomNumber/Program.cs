28f569cf73576d3a6533ba9c14ead3601ce8e641
RasmusR <rasmus.raadik@tktk.ee>
2023-03-01 14:47:06 +0200

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Random rnd = new Random();
+
+int myRandomNum;
+
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    myRandomNum = rnd.Next(0, 11);
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"Arv on: {myRandomNum}");
+}
+Console.WriteLine($"Summa: {randomSum}");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true;
+
+while(loopActive)
+{
+    cpuRandom = rnd.Next(1, 3);
+    Console.WriteLine("Enter a number 1-3: ");
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Nice");
+        loopActive = false;
+    }
+    else
+    {
+        Console.WriteLine("Try again");
+    }
+}
+Console.WriteLine("Hyva päeva");
